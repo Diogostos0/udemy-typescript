dbb95f8760934488448f6e04927b30f29e3187e9
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-05-29 17:09:01 -0300

    config: update config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,27 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPrettierConfig from 'eslint-config-prettier';
+import prettierConfig from './.prettierrc.js';
 
 export default tseslint.config(
-  // Config base do TypeScript
   {
     files: ['**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: true, // Habilita análise de tsconfig.json
-        sourceType: 'module', // Força módulos ESM
+        sourceType: 'module',
       },
-    },
-    rules: {
-      ...js.configs.recommended.rules,
     },
   },
 
-  // Integração com Prettier
   {
     plugins: {
       prettier: prettierPlugin,
     },
     rules: {
-      ...eslintConfigPrettier.rules,
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      ...eslintPrettierConfig.rules,
+      'prettier/prettier': ['error', prettierConfig],
     },
-  }
+  },
 );
